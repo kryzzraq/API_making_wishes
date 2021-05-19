e4858e2c62cda2c3e8fec913469692a283c9b2f5
Laura Martin <lauramartin@sipay.es>
2021-05-19 13:12:23 +0200

    added functin to login

--- src/config/db.php
+++ src/config/db.php
@@ -1,12 +1,13 @@
 <?php
-class Db {
+
+class DB {
   private $host = HOST_BD;
-  private $bbdd = BD;
   private $user = USER_BD;
   private $password = PW_BD;
+  private $bbdd = BD;
 
   public function connect(){
-    $cnn = new mysqli($this->$host, $this->$user, $this->$password, $this->$bbdd);
+    $cnn = new mysqli($this->host, $this->user, $this->password, $this->bbdd);
 
     if($cnn->connect_errno){
       return false;
--- src/routes/login.php
+++ src/routes/login.php
@@ -2,15 +2,47 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-
 require_once '../src/config/config.php';
 require_once '../src/config/db.php';
 
+$app->post('/login', function (Request $request, Response $response, array $args) {
 
-$app->get('/hola', function (Request $request, Response $response, array $args) {
-  $a = "Hola";
-  $response->getBody()->write($a);
+  $cnn = new DB();
+  $resp = '';
+
+  try{
+    $cnn = $cnn->connect();
+
+    if(!$cnn){
+      throw new Exception("Error al conectar con la base de datos.", 1);
+    }
+
+    $user = $request->getParam('email');
+    $pw= $request->getParam('password');    
+    $sql= "SELECT email,name,last_name_1,last_name_2,rol,route_image,id_user FROM Users WHERE email = ?";
+
+    $stmt = $cnn->prepare($sql);
+    $stmt->bind_param("s",$user);
+
+   
+    $stmt->bind_result($email,$name,$last_name_1,$last_name_2,$rol,$route_image,$id_user);
+    $stmt->execute();
+    $stmt->store_result();
+    
+
+    if($stmt->affected_rows!=1){
+      $stmt-> close();
+      $cnn-> close();
+      throw new Exception("Usuario o contraseña incorrectos.", 5);
+    }
+    
+    $stmt->fetch();
+    $resp = '{"email":"'.$email.'","name":"'.$name.'","last_name_1":"'.$last_name_1.'","last_name_2":"'.$last_name_2.'","rol":"'.$rol.'","route_image":"'.$route_image.'","id_user":"'.$id_user.'"}';
+
+  }catch (Exception $e){
+      $resp = '{"error": {"text":"'.$e-> getMessage().'"}}';
+  }
+  $response->getBody()->write($resp);
   $response->withHeader('Content-Type', 'application/json');
-  var_dump($response);
-  return $response;
+  return $response;      
 });
